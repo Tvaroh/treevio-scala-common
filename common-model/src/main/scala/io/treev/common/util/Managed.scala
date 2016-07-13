805c0e06a9f0e1af25ed9ae95a41931b98307ef0
Alexander Semenov <tvaroh@icloud.com>
2016-07-13 09:07:22 +0300

    Improve Managed.

--- common-model/src/main/scala/io/treev/common/util/Managed.scala
+++ common-model/src/main/scala/io/treev/common/util/Managed.scala
@@ -1,5 +1,10 @@
 package io.treev.common.util
 
+import cats.syntax.functor._
+import io.treev.common.concurrent.Async
+
+import scala.concurrent.{ExecutionContext, Future}
+
 object Managed {
 
   type AutoCloseableView[T] = T => AutoCloseable
@@ -10,5 +15,16 @@ object Managed {
     } finally {
       resource.close()
     }
+
+  def future[T : AutoCloseableView, V](resource: T)(op: T => Future[V])
+                                      (implicit ec: ExecutionContext): Future[V] = {
+    val future = op(resource)
+    future.onComplete(_ => resource.close())
+    future
+  }
+
+  def async[T : AutoCloseableView, V](resource: T)(op: T => Async[V])
+                                     (implicit ec: ExecutionContext): Async[V] =
+    op(resource).map(v => try v finally { resource.close() })
 
 }
