732116da2f1237987490167ba2d05aa77d412931
Alexander Semenov <tvaroh@icloud.com>
2016-07-22 10:47:42 +0300

    Minor code style fix.

--- common-impl/src/main/scala/io/treev/common/controller/JsonEncoders.scala
+++ common-impl/src/main/scala/io/treev/common/controller/JsonEncoders.scala
@@ -4,6 +4,6 @@ import io.circe._
 
 trait JsonEncoders {
 
-  implicit val printer: Json => String = Printer.noSpaces.copy(dropNullKeys = true).pretty _
+  implicit val printer: Json => String = Printer.noSpaces.copy(dropNullKeys = true).pretty
 
 }
