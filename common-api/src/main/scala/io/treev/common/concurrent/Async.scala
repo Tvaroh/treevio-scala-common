3ffce5b5aedf23ad172ebdb422e948c71b8744a8
Alexander Semenov <tvaroh@icloud.com>
2016-07-23 13:18:54 +0300

    Improve async instances.

--- common-api/src/main/scala/io/treev/common/concurrent/Async.scala
+++ common-api/src/main/scala/io/treev/common/concurrent/Async.scala
@@ -11,6 +11,9 @@ object Async extends AsyncInstances {
   def apply[A](run: () => Future[A]): Async[A] = new AsyncImpl(run)
   def apply[A](comp: => A)(implicit ec: ExecutionContext): Async[A] = new AsyncImpl(() => Future(comp))
 
+  def pure[A](a: A): Async[A] = new AsyncImpl(() => Future.successful(a))
+  def failed[A](t: Throwable): Async[A] = new AsyncImpl(() => Future.failed(t))
+
 }
 
 private class AsyncImpl[A](_run: () => Future[A]) extends Async[A] {
--- common-api/src/main/scala/io/treev/common/concurrent/AsyncInstances.scala
+++ common-api/src/main/scala/io/treev/common/concurrent/AsyncInstances.scala
@@ -1,19 +1,50 @@
 package io.treev.common.concurrent
 
 import cats._
+import cats.data.Xor
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.control.NonFatal
 
 trait AsyncInstances extends AsyncInstances1 {
 
-  implicit def asyncFunctor(implicit ec: ExecutionContext): Functor[Async] =
-    new AsyncFunctor
+  implicit def asyncInstance(implicit ec: ExecutionContext): MonadError[Async, Throwable] =
+    new AsyncCoflatMap with MonadError[Async, Throwable] {
 
-  implicit def asyncApplicative(implicit ec: ExecutionContext): Applicative[Async] =
-    new AsyncApplicative
+      override def pure[A](a: A): Async[A] =
+        Async.pure(a)
 
-  implicit def asyncMonad(implicit ec: ExecutionContext): Monad[Async] =
-    new AsyncMonad
+      override def pureEval[A](a: Eval[A]): Async[A] =
+        Async(() => a match {
+          case Now(x) => Future.successful(x)
+          case _ => Future(a.value)
+        })
+
+      override def map[A, B](fa: Async[A])(f: A => B): Async[B] =
+        Async(() => fa.run().map(f))
+
+      override def flatMap[A, B](fa: Async[A])(f: A => Async[B]): Async[B] =
+        Async(() => fa.run().flatMap(a => f(a).run()))
+
+      override def handleErrorWith[A](fa: Async[A])
+                                     (f: Throwable => Async[A]): Async[A] =
+        Async(() => fa.run().recoverWith { case NonFatal(t) => f(t).run() })
+
+      override def raiseError[A](t: Throwable): Async[A] =
+        Async.failed(t)
+
+      override def attempt[A](fa: Async[A]): Async[Xor[Throwable, A]] =
+        recover[Xor[Throwable, A]](map(fa)(Xor.right)) { case NonFatal(t) => Xor.left(t) }
+
+      override def recover[A](fa: Async[A])
+                             (pf: PartialFunction[Throwable, A]): Async[A] =
+        Async(() => fa.run().recover(pf))
+
+      override def recoverWith[A](fa: Async[A])
+                                 (pf: PartialFunction[Throwable, Async[A]]): Async[A] =
+        Async(() => fa.run().recoverWith { case NonFatal(t) => pf(t).run() })
+
+    }
 
   implicit def asyncGroup[A: Group](implicit ec: ExecutionContext): Group[Async[A]] =
     new AsyncGroup[A]
@@ -24,6 +55,7 @@ private[concurrent] trait AsyncInstances1 extends AsyncInstances2 {
 
   implicit def asyncMonoid[A: Monoid](implicit ec: ExecutionContext): Monoid[Async[A]] =
     new AsyncMonoid[A]
+
 }
 
 private[concurrent] trait AsyncInstances2 {
@@ -33,33 +65,25 @@ private[concurrent] trait AsyncInstances2 {
 
 }
 
-private class AsyncFunctor(implicit ec: ExecutionContext) extends Functor[Async] {
-  override def map[A, B](fa: Async[A])(f: A => B): Async[B] =
-    Async(() => fa.run().map(f))
-}
+private abstract class AsyncCoflatMap(implicit ec: ExecutionContext) extends CoflatMap[Async] {
 
-private class AsyncApplicative(implicit ec: ExecutionContext) extends Applicative[Async] {
-  override def pure[A](a: A): Async[A] =
-    Async(() => Future.successful(a))
-  override def pureEval[A](a: Eval[A]): Async[A] =
-    Async(() => Future(a.value))
-  override def ap[A, B](ff: Async[A => B])(fa: Async[A]): Async[B] =
-    Async(() => fa.run().flatMap(a => ff.run().map(_(a))))
-}
+  override def map[A, B](fa: Async[A])(f: A => B): Async[B] = Async(() => fa.run().map(f))
 
-private class AsyncMonad(implicit ec: ExecutionContext) extends AsyncApplicative with Monad[Async] {
-  override def flatMap[A, B](fa: Async[A])(f: A => Async[B]): Async[B] =
-    Async(() => fa.run().flatMap(a => f(a).run()))
+  override def coflatMap[A, B](fa: Async[A])(f: Async[A] => B): Async[B] = Async(() => Future(f(fa)))
+
 }
 
 private class AsyncGroup[A](implicit A: Group[A], ec: ExecutionContext) extends AsyncMonoid[A] with Group[Async[A]] {
+
   override def inverse(fa: Async[A]): Async[A] = {
     import cats.syntax.group._
     Async(() => fa.run().map(_.inverse))
   }
+
 }
 
 private class AsyncSemigroup[A: Semigroup](implicit ec: ExecutionContext) extends Semigroup[Async[A]] {
+
   override def combine(fx: Async[A], fy: Async[A]): Async[A] = {
     val xFuture = fx.run()
     val yFuture = fy.run()
@@ -67,9 +91,12 @@ private class AsyncSemigroup[A: Semigroup](implicit ec: ExecutionContext) extend
     import cats.syntax.semigroup._
     Async(() => xFuture.flatMap(x => yFuture.map(y => x |+| y)))
   }
+
 }
 
 private class AsyncMonoid[A: Monoid](implicit ec: ExecutionContext) extends AsyncSemigroup[A] with Monoid[Async[A]] {
+
   override def empty: Async[A] =
     Async(() => Future.successful(Monoid[A].empty))
+
 }
