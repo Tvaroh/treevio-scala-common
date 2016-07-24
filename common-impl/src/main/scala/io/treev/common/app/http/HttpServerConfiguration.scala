76153f9f22f1d7d6095d9803f392155450cbd530
Alexander Semenov <tvaroh@icloud.com>
2016-07-24 08:50:08 +0300

    Case class back.

--- common-impl/src/main/scala/io/treev/common/app/http/HttpServerConfiguration.scala
+++ common-impl/src/main/scala/io/treev/common/app/http/HttpServerConfiguration.scala
@@ -1,18 +1,5 @@
 package io.treev.common.app.http
 
-trait HttpServerConfiguration {
-  def serverId: String
-  def interface: String
-  def port: Int
-}
-
-object HttpServerConfiguration {
-  def apply(_serverId: String,
-            _interface: String,
-            _port: Int): HttpServerConfiguration =
-    new HttpServerConfiguration {
-      override def serverId: String = _serverId
-      override def interface: String = _interface
-      override def port: Int = _port
-    }
-}
+case class HttpServerConfiguration(serverId: String,
+                                   interface: String,
+                                   port: Int)
